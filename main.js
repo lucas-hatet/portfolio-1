b1089c0c0853a50e1116d542bc7071acbc812db2
hatet-lucas <lcs.hatet@gmail.com>
2024-11-30 19:39:52 +0100

    init

---
+++ main.js
@@ -0,0 +1,54 @@
+// Background menu on scroll
+
+const header = document.getElementById("menu");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 1) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
+
+
+
+// Welcome body background
+
+const updateBackgroundPosition = (xPercent, yPercent) => {
+    body.style.background = `radial-gradient(circle at ${xPercent}% ${yPercent}%, rgb(27, 48, 75), rgb(27, 38, 60), rgb(27, 28, 45))`;
+};
+
+const body = document.getElementById("body");
+
+document.addEventListener("mousemove", (event) => {
+    const xPercent = (event.clientX / window.innerWidth) * 100;
+    const yPercent = (event.clientY / window.innerHeight) * 100;
+    updateBackgroundPosition(xPercent, yPercent);
+});
+
+window.addEventListener("load", () => {
+    const xPercent = (window.innerWidth / 2) * 100 / window.innerWidth;
+    const yPercent = (window.innerHeight / 2) * 100 / window.innerHeight;
+    updateBackgroundPosition(xPercent, yPercent);
+});
+
+
+
+// Typing cursor
+
+const typingText1 = document.getElementById("typing-text-1");
+const typingText2 = document.getElementById("typing-text-2");
+const typingText3 = document.getElementById("typing-text-3");
+
+typingText2.classList.add("no-blink");
+typingText3.classList.add("no-blink");
+
+typingText1.addEventListener("animationend", () => {
+    typingText1.classList.add("no-blink");
+    typingText2.classList.remove("no-blink");
+});
+
+typingText2.addEventListener("animationend", () => {
+    typingText2.classList.add("no-blink");
+    typingText3.classList.remove("no-blink");
+});
---
+++ redirect.js
@@ -0,0 +1,3 @@
+document.getElementById("limenu").addEventListener("click", function(){
+    window.location = "index.html";
+});
