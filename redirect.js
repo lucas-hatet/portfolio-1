f500bc0ccf27c008095e5f262c3e35a30712dd14
hatet-lucas <lcs.hatet@gmail.com>
2024-12-01 11:27:52 +0100

    fichier experiences

--- redirect.js
+++ redirect.js
@@ -1,3 +1,7 @@
 document.getElementById("limenu").addEventListener("click", function(){
     window.location = "index.html";
 });
+
+document.getElementById("lixp").addEventListener("click", function(){
+    window.location = "experiences.html";
+});
